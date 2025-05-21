6e3ecc1b95b8aabfeb26e46d82ffefd9684394b8
synicaex <alexanderroskamp1@gmail.com>
2025-05-21 14:07:46 +0200

    start of levelSimulation

---
+++ src/main/java/mm/gui/simulation.java
@@ -0,0 +1,31 @@
+package mm.gui;
+
+import java.util.zip.CRC32;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class simulation extends Application{
+
+    public static void main(String[] args) { 
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+
+        
+        StackPane root = new StackPane();
+        
+
+    }
+
+
+
+}
