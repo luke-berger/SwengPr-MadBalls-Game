9c8a1801e24dd2190ad8ec48a0fead001744cdad
Luke Berger <luke.berger@student.uni-luebeck.de>
2025-05-26 16:39:32 +0200

    Deleted unneeded import

--- src/main/java/mm/gui/Gui.java
+++ src/main/java/mm/gui/Gui.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
