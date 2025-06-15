b756209ce00280b9deab5fe6c7229bde89c2c0ff
synicaex <alexanderroskamp1@gmail.com>
2025-06-15 03:35:01 +0200

    fixed some warning sregarrding dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,13 @@
  * The main module of the mm application.
  */
 module mm {
-    requires javafx.controls;
+    requires transitive javafx.controls;
     requires transitive javafx.graphics;
-    requires jbox2d.library;
+    requires transitive jbox2d.library;
 
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
+    requires transitive com.fasterxml.jackson.databind;
 
     requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.javafx;
