358aadb2804354706d2bc1afd61b68cbd5108940
bene <arneschroedie@gmail.com>
2025-06-25 13:23:01 +0200

    fixed naming convention at test

---
+++ src/test/java/mm/model/level/FakeTest.java
@@ -0,0 +1,19 @@
+package mm.model.level;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Testing the Example class with its arbitrary arithmetical computations
+ */
+public class FakeTest {
+    /**
+     * This tes case doesn't make any sense. It is just here as an example.
+     */
+    @Test
+    public void test1() {
+        assertTrue(true);
+        System.out.println("succes!");
+    }
+}
